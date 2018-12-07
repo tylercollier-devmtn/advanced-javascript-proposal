a8f63777456fd41def54b1eb6017d4d1f9d58088
Tyler Collier <tyler.collier+github@devmounta.in>
2018-12-07 12:11:28 -0700

    Add form inputs

--- src/App.js
+++ src/App.js
@@ -3,23 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      num1: '',
+      num2: '',
+      result: ''
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { num1, num2 } = this.state;
+    const biggest = this.getBiggest(num1, num2);
+    this.setState({ result: `The biggest is ${biggest}`});
+  }
+
+  getBiggest(a, b) {
+    // Your code goes here
+  }
+
   render() {
+    const { num1, num2, result } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Which is bigger?</h1>
+        <div>
+          <div>
+            Num 1: <input
+              value={num1}
+              onChange={event => this.setState({ num1: event.target.value })}
+            />
+          </div>
+          <div>
+            Num 2: <input
+              value={num2}
+              onChange={event => this.setState({ num2: event.target.value })}
+            />
+          </div>
+          <div><button onClick={this.handleClick}>Get biggest</button></div>
+          <div>{result}</div>
+        </div>
       </div>
     );
   }
